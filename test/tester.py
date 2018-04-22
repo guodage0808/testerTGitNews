cddb1a87e5952de7d3bbdc56681f669b90ee8b3b
xiaopeng.guo <2171029637@qq.com>
2018-04-22 13:10:00 +0800

    添加延迟

--- test/tester.py
+++ test/tester.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 driver = webdriver.Firefox()
 driver.get('http://www.baidu.com')
 
-driver.find_element_by_id("kw").send_keys(u"中国selenium")
+driver.find_element_by_id("kws").send_keys(u"中国selenium")
 time.sleep(5)
 #通过id定位输入框，html规定，id在文档中必须唯一，类似于我们的身份证号
 #driver.find_element_by_id("su").click()
