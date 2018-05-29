658f37dc7905e182b1d8d9071194dcd66695e4b1
xiaopeng.guo <2171029637@qq.com>
2018-05-29 21:58:11 +0800

    本次修改代码是为了演示上传git

--- test/tester.py
+++ test/tester.py
@@ -7,6 +7,7 @@ driver.get('http://www.baidu.com')
 
 driver.find_element_by_id("kw").send_keys(u"中国selenium")
 time.sleep(5)
+print ("1111")
 #通过id定位输入框，html规定，id在文档中必须唯一，类似于我们的身份证号
 #driver.find_element_by_id("su").click()
 #driver.quit()
